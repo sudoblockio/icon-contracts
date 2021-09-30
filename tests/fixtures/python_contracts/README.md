0ba12512033a8130eef4e7ad7f6dec3d060df1ce
robcxyz <rob@geometrylabs.io>
2021-09-29 18:29:33 -0700

    chore: preserve python parsing import test fail

---
+++ tests/fixtures/python_contracts/README.md
@@ -0,0 +1,4 @@
+
+Python contract imports have no plans to support based on https://github.com/geometry-labs/icon-contracts/issues/4
+
+Code is preserved here for future reference in case this changes.
---
+++ tests/fixtures/python_contracts/baln/importer.py
@@ -0,0 +1,13 @@
+# import sys
+# sys.path.append("..")
+
+# from typing import TypedDict
+# from balance import BalancedToken
+import os
+
+os.chdir("tokens")
+
+
+from tokens.IRC2 import IRC2
+
+print()
