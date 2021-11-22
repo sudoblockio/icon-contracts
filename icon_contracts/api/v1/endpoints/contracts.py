1507750add4c7a47bdd61977f1bc77d541428d1c
robcxyz <rob@geometrylabs.io>
2021-11-22 12:20:08 -0800

    feat: add total count header and restrcit queries to more sensible params

--- icon_contracts/api/v1/endpoints/contracts.py
+++ icon_contracts/api/v1/endpoints/contracts.py
@@ -1,10 +1,9 @@
 from http import HTTPStatus
 from typing import List
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, Response
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlmodel import select
-from starlette.responses import Response
+from sqlmodel import func, select
 
 from icon_contracts.api.db import get_session
 from icon_contracts.models.contracts import Contract
@@ -14,34 +13,46 @@ router = APIRouter()
 
 @router.get("/contracts")
 async def get_contracts(
+    response: Response,
     session: AsyncSession = Depends(get_session),
     skip: int = Query(0),
     limit: int = Query(100, gt=0, lt=101),
     contract_type: str = Query(None),
     status: str = Query(None),
-    last_updated_block: int = Query(None),
-    last_updated_timestamp: int = Query(None),
-    created_block: int = Query(None),
+    # last_updated_block: int = Query(None),
+    # last_updated_timestamp: int = Query(None),
+    # created_block: int = Query(None),
     created_timestamp: int = Query(None),
 ) -> List[Contract]:
     """Return list of contracts"""
 
     query = select(Contract).offset(skip).limit(limit)
+    query_count = select(func.count(Contract.address))
 
     if contract_type:
         query = query.where(Contract.contract_type == contract_type)
+        query_count = query.where(Contract.contract_type == contract_type)
     if status:
         query = query.where(Contract.status == status)
-    if last_updated_block:
-        query = query.where(Contract.last_updated_block == last_updated_block)
-    if last_updated_timestamp:
-        query = query.where(Contract.last_updated_timestamp == last_updated_timestamp)
-    if created_block:
-        query = query.where(Contract.created_block == created_block)
+        query_count = query.where(Contract.status == status)
+    # if last_updated_block:
+    #     query = query.where(Contract.last_updated_block == last_updated_block)
+    #     query_count = query.where(Contract.last_updated_block == last_updated_block)
+    # if last_updated_timestamp:
+    #     query = query.where(Contract.last_updated_timestamp == last_updated_timestamp)
+    #     query_count = query.where(Contract.last_updated_timestamp == last_updated_timestamp)
+    # if created_block:
+    #     query = query.where(Contract.created_block == created_block)
+    #     query_count = query.where(Contract.created_block == created_block)
     if created_timestamp:
         query = query.where(Contract.created_timestamp == created_timestamp)
+        query_count = query.where(Contract.created_timestamp == created_timestamp)
 
     result = await session.execute(query)
+    result_count = await session.execute(query_count)
+
+    response.headers["x-total-count"] = str(result_count.scalars().all()[0])
+
     contracts = result.scalars().all()
     return contracts
 
