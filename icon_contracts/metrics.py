ace712446615f77e5056128b6695f24cb20f0624
robcxyz <rob@geometrylabs.io>
2022-04-04 18:37:24 -0700

    chore: clean + build

--- icon_contracts/metrics.py
+++ icon_contracts/metrics.py
@@ -20,22 +20,3 @@ class Metrics:
             "max_block_number_transactions_raw",
             "The block height",
         )
-
-
-#
-#     def set(self, metric, value):
-#         metric = getattr(self, metric)
-#         metric.labels(settings.NETWORK_NAME).set(value)
-#
-#
-# if __name__ == '__main__':
-#     from prometheus_client import start_http_server
-#     from multiprocessing.pool import ThreadPool
-#
-#     metrics_pool = ThreadPool(1)
-#
-#     metrics_pool.apply_async(start_http_server, (settings.METRICS_PORT, settings.METRICS_ADDRESS))
-#     start_http_server(9401, "localhost")
-#
-#     m = Metrics()
-#     m.set("preps_created_python")
