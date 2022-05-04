09f0fc1767d240083811c43efd8b3a14795395cf
robcxyz <rob@geometrylabs.io>
2022-05-03 19:14:27 -0700

    fix: proto produce

--- icon_contracts/workers/kafka.py
+++ icon_contracts/workers/kafka.py
@@ -90,6 +90,9 @@ class Worker(BaseModel):
         # # Json producer for dead letter queues
         self.json_producer = Producer({"bootstrap.servers": self.kafka_server})
 
+        # TODO: RM this and make clean
+        self.protobuf_producer = self.json_producer
+
         if self.check_topics:
             admin_client = AdminClient({"bootstrap.servers": self.kafka_server})
             topics = admin_client.list_topics().topics
@@ -109,6 +112,15 @@ class Worker(BaseModel):
             self.json_producer.poll(1)
             self.json_producer.produce(topic=topic, value=value, key=key)
         self.json_producer.flush()
+
+    def produce_protobuf(self, topic, key, value):
+        try:
+            self.protobuf_producer.produce(topic=topic, value=value.SerializeToString(), key=key)
+            self.protobuf_producer.poll(0)
+        except BufferError:
+            self.protobuf_producer.poll(1)
+            self.protobuf_producer.produce(topic=topic, value=value.SerializeToString(), key=key)
+        self.protobuf_producer.flush()
 
     def start(self):
         self.consumer.subscribe([self.topic])
