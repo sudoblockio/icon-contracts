6be3be2a7504a02c375786a8a27dbab704697fcb
Lucas-L <lucas@staky.io>
2022-06-10 10:20:11 +0200

    NFT classifier

--- icon_contracts/core/classifier.py
+++ icon_contracts/core/classifier.py
@@ -77,6 +77,423 @@ IRC2_METHODS = [
     },
 ]
 
+IRC31_METHODS = [
+    {
+        "name": "ApprovalForAll",
+        "type": "eventlog",
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "Address",
+                "indexed": "0x1"
+            },
+            {
+                "name": "_operator",
+                "type": "Address",
+                "indexed": "0x1"
+            },
+            {
+                "name": "_approved",
+                "type": "bool"
+            }
+        ]
+    },
+    {
+        "name": "TransferBatch",
+        "type": "eventlog",
+        "inputs": [
+            {
+                "name": "_operator",
+                "type": "Address",
+                "indexed": "0x1"
+            },
+            {
+                "name": "_from",
+                "type": "Address",
+                "indexed": "0x1"
+            },
+            {
+                "name": "_to",
+                "type": "Address",
+                "indexed": "0x1"
+            },
+            {
+                "name": "_ids",
+                "type": "bytes"
+            },
+            {
+                "name": "_values",
+                "type": "bytes"
+            }
+        ]
+    },
+    {
+        "name": "TransferSingle",
+        "type": "eventlog",
+        "inputs": [
+            {
+                "name": "_operator",
+                "type": "Address",
+                "indexed": "0x1"
+            },
+            {
+                "name": "_from",
+                "type": "Address",
+                "indexed": "0x1"
+            },
+            {
+                "name": "_to",
+                "type": "Address",
+                "indexed": "0x1"
+            },
+            {
+                "name": "_id",
+                "type": "int"
+            },
+            {
+                "name": "_value",
+                "type": "int"
+            }
+        ]
+    },
+    {
+        "name": "URI",
+        "type": "eventlog",
+        "inputs": [
+            {
+                "name": "_id",
+                "type": "int",
+                "indexed": "0x1"
+            },
+            {
+                "name": "_value",
+                "type": "str"
+            }
+        ]
+    },
+    {
+        "name": "balanceOf",
+        "type": "function",
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "Address"
+            },
+            {
+                "name": "_id",
+                "type": "int"
+            }
+        ],
+        "outputs": [
+            {
+                "type": "int"
+            }
+        ],
+        "readonly": "0x1"
+    },
+    {
+        "name": "balanceOfBatch",
+        "type": "function",
+        "inputs": [
+            {
+                "name": "_owners",
+                "type": "[]Address"
+            },
+            {
+                "name": "_ids",
+                "type": "[]int"
+            }
+        ],
+        "outputs": [
+            {
+                "type": "list"
+            }
+        ],
+        "readonly": "0x1"
+    },
+    {
+        "name": "isApprovedForAll",
+        "type": "function",
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "Address"
+            },
+            {
+                "name": "_operator",
+                "type": "Address"
+            }
+        ],
+        "outputs": [
+            {
+                "type": "bool"
+            }
+        ],
+        "readonly": "0x1"
+    },
+    {
+        "name": "name",
+        "type": "function",
+        "inputs": [],
+        "outputs": [
+            {
+                "type": "str"
+            }
+        ],
+        "readonly": "0x1"
+    },
+    {
+        "name": "setApprovalForAll",
+        "type": "function",
+        "inputs": [
+            {
+                "name": "_operator",
+                "type": "Address"
+            },
+            {
+                "name": "_approved",
+                "type": "bool"
+            }
+        ],
+        "outputs": []
+    },
+    {
+        "name": "tokenURI",
+        "type": "function",
+        "inputs": [
+            {
+                "name": "_id",
+                "type": "int"
+            }
+        ],
+        "outputs": [
+            {
+                "type": "str"
+            }
+        ],
+        "readonly": "0x1"
+    },
+    {
+        "name": "transferFrom",
+        "type": "function",
+        "inputs": [
+            {
+                "name": "_from",
+                "type": "Address"
+            },
+            {
+                "name": "_to",
+                "type": "Address"
+            },
+            {
+                "name": "_id",
+                "type": "int"
+            },
+            {
+                "name": "_value",
+                "type": "int"
+            },
+            {
+                "name": "_data",
+                "default": null,
+                "type": "bytes"
+            }
+        ],
+        "outputs": []
+    },
+    {
+        "name": "transferFromBatch",
+        "type": "function",
+        "inputs": [
+            {
+                "name": "_from",
+                "type": "Address"
+            },
+            {
+                "name": "_to",
+                "type": "Address"
+            },
+            {
+                "name": "_ids",
+                "type": "[]int"
+            },
+            {
+                "name": "_values",
+                "type": "[]int"
+            },
+            {
+                "name": "_data",
+                "default": null,
+                "type": "bytes"
+            }
+        ],
+        "outputs": []
+    }
+]
+
+IRC3_MEHTHODS = [
+    {
+        "name": "Approval",
+        "type": "eventlog",
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "Address",
+                "indexed": "0x1"
+            },
+            {
+                "name": "_approved",
+                "type": "Address",
+                "indexed": "0x1"
+            },
+            {
+                "name": "_tokenId",
+                "type": "int",
+                "indexed": "0x1"
+            }
+        ]
+    },
+    {
+        "name": "Transfer",
+        "type": "eventlog",
+        "inputs": [
+            {
+                "name": "_from",
+                "type": "Address",
+                "indexed": "0x1"
+            },
+            {
+                "name": "_to",
+                "type": "Address",
+                "indexed": "0x1"
+            },
+            {
+                "name": "_tokenId",
+                "type": "int",
+                "indexed": "0x1"
+            }
+        ]
+    },
+    {
+        "name": "approve",
+        "type": "function",
+        "inputs": [
+            {
+                "name": "_to",
+                "type": "Address"
+            },
+            {
+                "name": "_tokenId",
+                "type": "int"
+            }
+        ],
+        "outputs": []
+    },
+    {
+        "name": "getApproved",
+        "type": "function",
+        "inputs": [
+            {
+                "name": "_tokenId",
+                "type": "int"
+            }
+        ],
+        "outputs": [
+            {
+                "type": "Address"
+            }
+        ],
+        "readonly": "0x1"
+    },
+    {
+        "name": "name",
+        "type": "function",
+        "inputs": [],
+        "outputs": [
+            {
+                "type": "str"
+            }
+        ],
+        "readonly": "0x1"
+    },
+    {
+        "name": "balanceOf",
+        "type": "function",
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "Address"
+            }
+        ],
+        "outputs": [
+            {
+                "type": "int"
+            }
+        ],
+        "readonly": "0x1"
+    },
+    {
+        "name": "ownerOf",
+        "type": "function",
+        "inputs": [
+            {
+                "name": "_tokenId",
+                "type": "int"
+            }
+        ],
+        "outputs": [
+            {
+                "type": "Address"
+            }
+        ],
+        "readonly": "0x1"
+    },
+    {
+        "name": "symbol",
+        "type": "function",
+        "inputs": [],
+        "outputs": [
+            {
+                "type": "str"
+            }
+        ],
+        "readonly": "0x1"
+    },
+    {
+        "name": "transfer",
+        "type": "function",
+        "inputs": [
+            {
+                "name": "_to",
+                "type": "Address"
+            },
+            {
+                "name": "_tokenId",
+                "type": "int"
+            }
+        ],
+        "outputs": []
+    },
+    {
+        "name": "transferFrom",
+        "type": "function",
+        "inputs": [
+            {
+                "name": "_from",
+                "type": "Address"
+            },
+            {
+                "name": "_to",
+                "type": "Address"
+            },
+            {
+                "name": "_tokenId",
+                "type": "int"
+            }
+        ],
+        "outputs": []
+    }
+]
 
 def is_irc2(abi: List[dict], address: str) -> bool:
     try:
