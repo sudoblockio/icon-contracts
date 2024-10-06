902dbe06f0799423a4fae4e29c370e9b476b3a71
shreyasbhat0 <ks.shreyas0@gmail.com>
2024-10-06 17:24:08 +0530

    fix: add tags on router

--- icon_contracts/api/v1/router.py
+++ icon_contracts/api/v1/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
 from icon_contracts.api.v1.endpoints import contracts
 
-api_router = APIRouter()
+api_router = APIRouter(tags=["contracts"])
 api_router.include_router(contracts.router)
